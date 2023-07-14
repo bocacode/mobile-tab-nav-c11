f93263d1f0fff55231a7b7060321033d7d200a2c
Todd H. Albert <todd@bocacode.com>
2023-07-14 10:03:58 -0400

    custom tab navigation

---
+++ app/(tabs)/albums.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "@/components/Themed";
+
+export default function Albums() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ fontSize: 24 }}>Albums</Text>
+    </View>
+  )
+}
